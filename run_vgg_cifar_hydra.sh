a6609b3cb5048f815921b9e97d2ef100b59c1fcd
Pierre Wolinski <pierre.wolinski@inria.fr>
2024-09-28 10:08:54 +0200

    UniformAvg on VGG

--- run_vgg_cifar_hydra.sh
+++ run_vgg_cifar_hydra.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 parent_dir='./data/outputs'
-expe_series='LeNet_CIFAR_Hydra_debug_01'
+expe_series='VGG_CIFAR_Hydra_debug_01'
 
 module purge
 module load pytorch-gpu/py3/2.3.0
 
 
 HYDRA_FULL_ERROR=1 OC_CAUSE=1 python main_hydra.py --multirun hydra/launcher=submitit_slurm\
-                seed=1\
 				parent_dir="${parent_dir}"\
 				expe_series="${expe_series}"\
+                seed=70478166\
 				system.dtype=32\
-        		model.name='LeNet'\
-	        	model.args='6-16-120-84-10'\
-				model.act_function='tanh'\
+	        	model.name='VGG'\
+		        model.args='A'\
+				model.act_function='elu'\
 				model.scaling=False\
-				model.init.sigma_w=1.\
+				model.init.sigma_w=1.4142\
 				model.init.sigma_b=0.\
 				dataset.name='CIFAR10'\
 				dataset.path='/gpfswork/rech/tza/uki35ex/dataset'\
@@ -38,13 +38,13 @@ HYDRA_FULL_ERROR=1 OC_CAUSE=1 python main_hydra.py --multirun hydra/launcher=sub
 				optimizer.hg.momentum=.9\
 				optimizer.hg.momentum_damp=.9\
 				optimizer.hg.mom_lrs=0.\
-				optimizer.hg.period_hg=5\
+				optimizer.hg.period_hg=10\
 				optimizer.hg.nesterov.use=True\
 				optimizer.hg.nesterov.damping_int=1.\
 				optimizer.hg.remove_negative=True\
         		optimizer.hg.uniform_avg.use=True\
-        		optimizer.hg.uniform_avg.period=5\
-        		optimizer.hg.uniform_avg.warmup=5\
+        		optimizer.hg.uniform_avg.period=45\
+        		optimizer.hg.uniform_avg.warmup=45\
 				optimizer.hg.dmp_auto.use=True\
 				optimizer.hg.dmp_auto.patience=2\
 				optimizer.hg.dmp_auto.threshold=.0001\
