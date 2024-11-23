4b31bacb94e15e10d21144d5bea5fdb0207dc0e0
Pierre Wolinski <pierre.wolinski@inria.fr>
2024-11-23 10:35:23 +0100

    up

--- run_lenet_cifar_hydra.sh
+++ run_lenet_cifar_hydra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 parent_dir='/gpfswork/rech/tza/uki35ex/_Experiments/GroupedNewton_Results'
-expe_series='LeNet_CIFAR_UnifAvg_01_grid'
+expe_series='LeNet_CIFAR_ICLR25_01_test'
 
 module purge
 module load pytorch-gpu/py3/2.3.0
@@ -28,28 +28,28 @@ HYDRA_FULL_ERROR=1 OC_CAUSE=1 python main_hydra.py --multirun hydra/launcher=sub
 				logs_hg.use=False\
 				logs_hg.batch_size=1000\
 				logs_hg.test_float=False\
-				optimizer.epochs=100\
-				optimizer.name='NewtonSummaryUniformAvg'\
-				optimizer.lr=.001\
+				optimizer.epochs=200\
+				optimizer.name='NewtonSummary'\
+				optimizer.lr=.003\
 				optimizer.weight_decay=0.\
 				optimizer.momentum=.9\
 				optimizer.hg.batch_size=200\
 				optimizer.hg.optimizer='SGD'\
 				optimizer.hg.partition='canonical'\
-				optimizer.hg.damping=1.,.3,.1,.03\
+				optimizer.hg.damping=1.\
 				optimizer.hg.damping_schedule='None'\
 				optimizer.hg.momentum=.9\
 				optimizer.hg.momentum_damp=.9\
-				optimizer.hg.mom_lrs=0.\
+				optimizer.hg.mom_lrs=0.9\
 				optimizer.hg.period_hg=10\
 				optimizer.hg.nesterov.use=True\
-				optimizer.hg.nesterov.damping_int=3.,1.,.3,.1\
+				optimizer.hg.nesterov.damping_int=10.\
 				optimizer.hg.remove_negative=True\
-        		optimizer.hg.uniform_avg.use=True\
+        		optimizer.hg.uniform_avg.use=False\
         		optimizer.hg.uniform_avg.period=10\
         		optimizer.hg.uniform_avg.warmup=10\
 				optimizer.hg.dmp_auto.use=True\
-				optimizer.hg.dmp_auto.patience=2\
+				optimizer.hg.dmp_auto.patience=5\
 				optimizer.hg.dmp_auto.threshold=.0001\
 				optimizer.hg.dmp_auto.factor=.5\
 				optimizer.kfac.stat_decay=.95\
--- run_vgg_cifar_hydra.sh
+++ run_vgg_cifar_hydra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 parent_dir='/gpfswork/rech/tza/uki35ex/_Experiments/GroupedNewton_Results'
-expe_series='VGG_CIFAR_UnifAvg_01_final'
+expe_series='VGG_CIFAR_ICLR25_01_test'
 
 module purge
 module load pytorch-gpu/py3/2.3.0
@@ -13,7 +13,7 @@ HYDRA_FULL_ERROR=1 OC_CAUSE=1 python main_hydra.py --multirun hydra/launcher=sub
         		hydra.launcher.qos='qos_gpu-t3'\
 				parent_dir="${parent_dir}"\
 				expe_series="${expe_series}"\
-                seed=571677914,421146319,505702709,70478166,358472055\
+                seed=571677914\
 				system.dtype=32\
 	        	model.name='VGG'\
 		        model.args='A'\
@@ -29,8 +29,8 @@ HYDRA_FULL_ERROR=1 OC_CAUSE=1 python main_hydra.py --multirun hydra/launcher=sub
 				logs_hg.batch_size=1000\
 				logs_hg.test_float=False\
 				optimizer.epochs=100\
-				optimizer.name='NewtonSummaryUniformAvg'\
-				optimizer.lr=.001\
+				optimizer.name='NewtonSummary'\
+				optimizer.lr=.003\
 				optimizer.weight_decay=0.\
 				optimizer.momentum=.9\
 				optimizer.hg.batch_size=200\
@@ -43,15 +43,15 @@ HYDRA_FULL_ERROR=1 OC_CAUSE=1 python main_hydra.py --multirun hydra/launcher=sub
 				optimizer.hg.mom_lrs=0.\
 				optimizer.hg.period_hg=10\
 				optimizer.hg.nesterov.use=True\
-				optimizer.hg.nesterov.damping_int=.3\
+				optimizer.hg.nesterov.damping_int=3.\
 				optimizer.hg.remove_negative=True\
-        		optimizer.hg.uniform_avg.use=True\
+        		optimizer.hg.uniform_avg.use=False\
         		optimizer.hg.uniform_avg.period=5\
         		optimizer.hg.uniform_avg.warmup=5\
 				optimizer.hg.dmp_auto.use=True\
-				optimizer.hg.dmp_auto.patience=2\
+				optimizer.hg.dmp_auto.patience=0\
 				optimizer.hg.dmp_auto.threshold=.0001\
-				optimizer.hg.dmp_auto.factor=.5\
+				optimizer.hg.dmp_auto.factor=.8\
 				optimizer.kfac.stat_decay=.95\
 				optimizer.kfac.damping=.03\
 				optimizer.kfac.kl_clip=.01\
