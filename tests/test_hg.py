9090f7fef655f88ff4b6b1e011b8eb318a9ef392
Pierre Wolinski <pierre.wolinski@dauphine.psl.eu>
2024-11-29 20:11:20 +0100

    tests on hg: partially done

--- tests/test_hg.py
+++ tests/test_hg.py
@@ -27,12 +27,25 @@ def degree():
 def polynomial(degree):
     return Polynomial(degree)
 
+@pytest.fixture
+def fan_in():
+    return 7
+
+@pytest.fixture
+def fan_out():
+    return 4
+
+@pytest.fixture
+def linear(fan_in, fan_out):
+    return torch.nn.Linear(fan_in, fan_out)
+
 def _compute_Hg_pytorch(full_loss, x, y, tup_params):
     D = len(tup_params)
 
     # Order 1
     loss = full_loss(x, y)
     g2_tup = torch.autograd.grad(loss, tup_params, create_graph = True, materialize_grads = True)
+    g2_tup = tuple(g.sum() for g in g2_tup)
     g2 = torch.stack([g.detach() for g in g2_tup])
 
     # Order 2
@@ -42,18 +55,19 @@ def _compute_Hg_pytorch(full_loss, x, y, tup_params):
     order32 = torch.zeros(D, D, D)
     for i, g in enumerate(g2_tup):
         if not g.requires_grad:
-            h = tuple(torch.tensor(0.) for j in range(D))
+            h = torch.zeros(D)
         else:
             h = torch.autograd.grad(g, tup_params, create_graph = True, materialize_grads = True)
+            h = torch.stack([h_.sum() for h_ in h])
 
         for j in range(D):
-            H2[i,j] = h[j].sum().detach()
-            H2[j,i] = h[j].sum().detach()
+            H2[i,j] = h[j].detach()
+            H2[j,i] = h[j].detach()
             if not h[j].requires_grad:
                 k = 0.
             else:
                 k = torch.autograd.grad(h[j], tup_params, retain_graph = True, materialize_grads = True)
-                k = torch.stack(k).detach()
+                k = torch.stack([k_.detach().sum() for k_ in k])
             order32[i,j,:] = k
             order32[j,i,:] = k
             order32[i,:,j] = k
@@ -154,6 +168,56 @@ def test_Hg_polynomial_trivial_with_loss(polynomial, degree):
     H1, g1, order31 = compute_Hg(param_groups, full_loss, x, y, direction)
     
     H1p, g1p, order31p = compute_Hg(ParamGroups(build_partition.canonical(polynomial)[0]), full_loss, x, y, direction)
+
+    # Compute the derivatives with pytorch
+    H2, g2, order32 = _compute_Hg_pytorch(full_loss, x, y, param_groups.tup_params)
+    g2 = torch.sum(g2, (0,), keepdim = True)
+    H2 = torch.sum(H2, (0, 1), keepdim = True)
+    order32 = torch.sum(order32).unsqueeze(dim = 0)
+
+    assert torch.allclose(g1, g2)
+    assert torch.allclose(H1, H2)
+    assert torch.allclose(order31, order32)
+
+def test_Hg_linear_canonical(linear, fan_in, fan_out):
+    batch_size = 10
+    x = torch.randn(batch_size, fan_in)
+    y = torch.randn(batch_size, fan_out)
+
+    pgroups, name_groups = build_partition.canonical(linear)
+    param_groups = ParamGroups(pgroups)
+
+    def full_loss(x_, y_): 
+        return (linear(x_).sin() - y_).pow(2).sum()
+
+    # Compute the derivatives with the custom functions of grnewt
+    direction = (torch.ones(fan_out, fan_in), torch.ones(fan_out))
+    H1, g1, order31 = compute_Hg(param_groups, full_loss, x, y, direction)
+
+    # Compute the derivatives with pytorch
+    H2, g2, order32_ = _compute_Hg_pytorch(full_loss, x, y, param_groups.tup_params)
+    order32 = torch.zeros(2)
+    for i in range(2):
+        order32[i] = order32_[i,i,i]
+
+    assert torch.allclose(g1, g2)
+    assert torch.allclose(H1, H2)
+    assert torch.allclose(order31, order32)
+
+def test_Hg_linear_trivial(linear, fan_in, fan_out):
+    batch_size = 10
+    x = torch.randn(batch_size, fan_in)
+    y = torch.randn(batch_size, fan_out)
+
+    pgroups, name_groups = build_partition.trivial(linear)
+    param_groups = ParamGroups(pgroups)
+
+    def full_loss(x_, y_): 
+        return (linear(x_).sin() - y_).pow(2).sum()
+
+    # Compute the derivatives with the custom functions of grnewt
+    direction = (torch.ones(fan_out, fan_in), torch.ones(fan_out))
+    H1, g1, order31 = compute_Hg(param_groups, full_loss, x, y, direction)
 
     # Compute the derivatives with pytorch
     H2, g2, order32 = _compute_Hg_pytorch(full_loss, x, y, param_groups.tup_params)
