33784eb49eace3cbc74905a3bff0a146c5fa75c2
Pierre Wolinski <pierre.wolinski@dauphine.psl.eu>
2026-08-17 18:02:50 +0200

    debugging hydra

---
+++ _old_hydra/main_hydra.py
@@ -0,0 +1,20 @@
+import hydra
+from training_hydra import Trainer
+
+
+@hydra.main(version_base=None, config_path="./configs", config_name="config_hydra")
+def main(cfg):
+    """
+    try:
+        trainer = ctx.logger.load_checkpoint(log_name = 'last_ckpt')
+        print("Loading from latest checkpoint")
+    except:
+        print("Failed to load checkpoint, Starting from scratch")
+        trainer = Trainer(ctx.config, ctx.logger)
+    """
+    trainer = Trainer(cfg, hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
