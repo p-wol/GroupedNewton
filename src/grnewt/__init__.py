62c32d7b68ee689f437481437df81e33430e0a85
Pierre Wolinski <pierre.wolinski@inria.fr>
2026-08-20 18:38:43 +0200

    work in progress on compute_Hg, batched version 2x slower

--- src/grnewt/__init__.py
+++ src/grnewt/__init__.py
@@ -18,7 +18,7 @@ from . import datasets, models, optimizers, partition
 from .param_struct import ParamStructure
 from .differential import diff_n, diff_n_fullbatch
 from .hg import compute_Hg, compute_Hg_fullbatch
-from .hg_batched import compute_Hg_batched
+#from .hg_batched import compute_Hg_batched
 from .nesterov import nesterov_lrs
 from .newton_stochastic_hv import NewtonStochasticHv
 from .newton_summary import NewtonSummary
